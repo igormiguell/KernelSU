52054b55b672a5f84241edc0b0ff500474bcd7bc
igor <134963561+igormiguell@users.noreply.github.com>
2025-01-28 13:26:33 -0300

    Update pt_BR.ts

--- website/docs/.vitepress/locales/pt_BR.ts
+++ website/docs/.vitepress/locales/pt_BR.ts
@@ -47,7 +47,7 @@ function sidebarGuide() {
           { text: 'O que é KernelSU?', link: '/pt_BR/guide/what-is-kernelsu' },
           { text: 'Diferenças com Magisk', link: '/pt_BR/guide/difference-with-magisk' },
           { text: 'Instalação', link: '/pt_BR/guide/installation' },
-          { text: 'Como compilar?', link: '/pt_BR/guide/how-to-build' },
+          { text: 'Como compilar', link: '/pt_BR/guide/how-to-build' },
           { text: 'Integração para dispositivos não GKI', link: '/pt_BR/guide/how-to-integrate-for-non-gki'},
           { text: 'Dispositivos com suporte não oficial', link: '/pt_BR/guide/unofficially-support-devices.md' },
           { text: 'Guias de módulo', link: '/pt_BR/guide/module.md' },
